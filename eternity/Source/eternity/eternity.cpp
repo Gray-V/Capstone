ecc09aa8d6ea8a396f4571e2862a950536fa0d47
Grayson von Goetz <jvongoetz808@gmail.com>
2025-12-07 16:55:02 -0800

    test

--- eternity/Source/eternity/eternity.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "eternity.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, eternity, "eternity" );
-
-DEFINE_LOG_CATEGORY(Logeternity)
